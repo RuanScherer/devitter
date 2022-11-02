3132eeaa8e11ce80cbe885c7dbd6fecb308f3d2c
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2022-11-02 14:45:59 -0300

    feat: home page

--- index.php
+++ index.php
@@ -16,10 +16,34 @@
     <script src="scripts/tailwind-config.js"></script>
   </head>
 
-  <body class="bg-gray-900 text-primary-900">
-    <div class="container mx-auto">
-      <a href="view/login.php">Login</a>
-      <a href="view/register.php">Cadastre-se</a>
-    </div>
+  <body class="bg-gray-900">
+    <main
+      class="h-screen w-100"
+      style="background-image: url('assets/images/home-cover.jpg')"
+    >
+      <div class="h-screen w-100 p-8 bg-neutral-900 bg-opacity-80 flex flex-col items-center justify-center text-center">
+        <h1 class="text-emerald-400 font-bold text-4xl">
+          Fala, Dev!
+        </h1>
+        <h2 class="text-emerald-100 text-xl mt-2">
+          Inspire-se e compartilhe ideias em uma rede desenvolvida especialmente para devs.
+        </h2>
+
+        <nav class="flex gap-4 mt-8">
+          <a
+            href="view/login.php"
+            class="px-6 py-2 bg-emerald-500/20 text-neutral-50/75 text-lg font-medium rounded-lg hover:bg-emerald-500/30 hover:text-neutral-100 transition"
+          >
+            Entrar
+          </a>
+          <a
+            href="view/register.php"
+            class="px-6 py-2 bg-emerald-500 text-lg text-neutral-50 font-medium rounded-lg hover:brightness-90 transition"
+          >
+            Cadastrar-se
+          </a>
+        </nav>
+      </div>
+    </main>
   </body>
 </html>
