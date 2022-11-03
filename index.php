51eca4c755c48992ee80390f07300430ae9fb6e9
Ruan Scherer <ruan.scherer@paytrack.com.br>
2022-11-03 18:11:13 -0300

    feat: login page

--- index.php
+++ index.php
@@ -22,11 +22,16 @@
       style="background-image: url('assets/images/home-cover.jpg')"
     >
       <div class="h-screen w-100 p-8 bg-neutral-900 bg-opacity-80 flex flex-col items-center justify-center text-center">
+        <div class="flex items-center gap-3 mb-8">
+          <img class="w-9" src="assets/images/bird.svg" alt="Bird">
+          <p class="text-xl text-neutral-50">Devitter<span class="text-emerald-500">.</span></p>
+        </div>
+
         <h1 class="text-emerald-400 font-bold text-4xl">
           Fala, Dev!
         </h1>
         <h2 class="text-emerald-100 text-xl mt-2">
-          Inspire-se e compartilhe ideias em uma rede desenvolvida especialmente para devs.
+          Inspire-se e compartilhe ideias em uma rede feita especialmente para devs.
         </h2>
 
         <nav class="flex gap-4 mt-8">
@@ -38,7 +43,7 @@
           </a>
           <a
             href="view/register.php"
-            class="px-6 py-2 bg-emerald-500 text-lg text-neutral-50 font-medium rounded-lg hover:bg-emerald-500/80 transition"
+            class="px-6 py-2 bg-emerald-500 text-lg text-gray-900 font-medium rounded-lg hover:bg-emerald-500/80 transition"
           >
             Cadastrar-se
           </a>
