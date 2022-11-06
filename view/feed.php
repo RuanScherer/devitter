78a08df17212fc42f0045a632774a1c1d1b14d45
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2022-11-06 09:34:30 -0300

    feat: feed basic structure

--- view/feed.php
+++ view/feed.php
@@ -15,33 +15,161 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="scripts/tailwind-config.js"></script>
+    <script src="https://kit.fontawesome.com/0fc61897ab.js" crossorigin="anonymous"></script>
   </head>
 
-  <body class="bg-gray-900 text-emerald-500">
-    <div class="min-h-screen h-100 w-100 p-8">
-        <div class="flex items-center gap-3">
-            <img class="w-9" src="../assets/images/bird.svg" alt="Bird">
-            <label class="relative block grid col-span-2">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <p class="text-xl">#</p>
-                </span>
-                <input
-                  type="text"
-                  name="search"
-                  placeholder="Explore"
-                  class="rounded-md w-full border-solid bg-gray-800/75 focus:outline-none focus:border-emerald-500 focus:ring-1 focus:ring-emerald-500 py-1 pl-10 px-4 text-slate-200 transition"
-                />
-           </label>
-        </div> 
-        <div>
-            <button class="block h-6 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white">
-                <img class="h-full w-full object-cover" src="../assets/images/bird.svg" alt="">
+  <body class="bg-gray-900">
+    <div class="container mx-auto p-8">
+      <header class="flex items-center justify-between gap-3">
+        <img
+          class="w-9 hidden sm:block"
+          src="../assets/images/bird.svg"
+          alt="Bird"
+        />
+
+        <form method="POST">
+          <label class="relative block grid col-span-2">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <i class="fa-solid fa-hashtag text-emerald-500"></i>
+            </span>
+            <input
+              type="text"
+              name="search"
+              placeholder="Explore"
+              class="rounded-md w-full border-solid bg-gray-800/75 focus:outline-none focus:border-emerald-500 focus:ring-1 focus:ring-emerald-500 py-2 pl-9 px-4 text-slate-200 transition"
+            />
+          </label>
+        </form>
+
+        <div class="relative">
+          <button
+            class="w-10 h-10 rounded-full border-2 border-emerald-400 focus:outline-none bg-cover bg-center"
+            style="background-image: url('https://avatars.githubusercontent.com/u/50061559?v=4');"
+            onclick="toggleUserPopup()"
+          ></button>
+          
+          <nav
+            id="user-popup"
+            class="hidden absolute top-10 right-0 bg-gray-700 font-semibold rounded-lg py-2 w-32 mt-2 shadow-lg"
+          >
+            <a href="profile.php" class="block px-4 py-2 text-neutral-100 hover:bg-emerald-500/10">
+              Meu Perfil
+            </a>
+            <a href="#" class="block px-4 py-2 text-red-500 hover:bg-emerald-500/10">
+              Sair
+            </a>
+          </nav>
+        </div>
+      </header>
+
+      <div class="grid grid-cols-12 gap-8 mt-8">
+        <aside class="hidden h-fit lg:block lg:col-span-4 xl:col-span-3 bg-gray-800/75 rounded-lg shadow">
+          <div class="bg-emerald-500 h-24 rounded-t-lg"></div>
+
+          <div class="p-4">
+            <div
+              class="w-20 h-20 -mt-14 mx-auto rounded-full border-2 border-neutral-300 bg-cover bg-center"
+              style="background-image: url('https://avatars.githubusercontent.com/u/50061559?v=4');"
+            ></div>
+
+            <h2 class="text-2xl text-center text-neutral-100 font-semibold mt-2">
+              Ruan Scherer
+            </h2>
+            <h3 class="text-md text-center text-neutral-300 leading-none mb-8">
+              @ruan_scherer
+            </h3>
+
+            <a
+              href="view/register.php"
+              class="block px-6 py-2 bg-neutral-500/10 text-neutral-50/75 text-center text-md font-medium rounded-lg hover:bg-emerald-300/10 hover:text-neutral-100 transition"
+            >
+              Meu perfil
+            </a>
+          </div>
+        </aside>
+
+        <main class="col-span-12 lg:col-span-8 xl:col-span-9">
+          <form
+            method="POST"
+            class="flex flex-col items-stretch bg-gray-800/75 rounded-lg shadow"
+          >
+            <textarea
+              name="post_content"
+              placeholder="O que você está pensando?"
+              rows="3"
+              class="rounded-lg m-3 p-6 block bg-gray-700/30 text-neutral-100 hover:bg-gray-700/50 focus:bg-gray-700/50 focus:outline-none caret-emerald-500 resize-none transition"
+            ></textarea>
+            <button
+              type="submit"
+              class="mr-3 mb-3 px-6 py-2 self-end bg-emerald-500 text-md text-gray-900 font-medium rounded-lg hover:bg-emerald-500/80 transition"
+            >
+              Publicar
             </button>
-            <div class="bg-white rounded-lg py-2 w-32 mt-2 shadow-xl">
-                <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Meu Perfil</a>
-                <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">Sair</a>
-            </div>
-        </div> 
-    </div>     
+          </form>
+
+          <section class="flex flex-col items-stretch mt-4 bg-gray-800/75 rounded-lg shadow">
+            <article class="px-4 py-6 border-b border-slate-700 text-neutral-200 last:border-b-0">
+              <div class="flex items-center gap-3">
+                <a
+                  href="user.php?user="
+                  class="w-8 h-8 rounded-full focus:outline-none bg-cover bg-center"
+                  style="background-image: url('https://avatars.githubusercontent.com/u/50061559?v=4');"
+                ></a>
+
+                <h4 class="text-lg font-semibold">
+                  Ruan Scherer
+                </h4>
+              </div>
+              
+              <p class="mt-3 leading-tight">
+                Fala, pessoal! Vocês viram o lançamento da nova versão do Next.js??? A versão 13 do framework React vem 
+                com tudo e promete muito.
+              </p>
+
+              <a
+                href="post.php?post="
+                class="block w-fit px-4 mt-3 py-1.5 bg-neutral-500/10 text-neutral-50/75 text-sm font-medium rounded-lg hover:bg-emerald-300/10 hover:text-neutral-100 transition"
+              >
+                <i class="fa-regular fa-comment mr-1"></i>
+                Comentar
+              </a>
+            </article>
+            <article class="px-4 py-6 border-b border-slate-700 text-neutral-200 last:border-b-0">
+              <div class="flex items-center gap-3">
+                <a
+                  href="user.php?user="
+                  class="w-8 h-8 rounded-full focus:outline-none bg-cover bg-center"
+                  style="background-image: url('https://avatars.githubusercontent.com/u/50061559?v=4');"
+                ></a>
+
+                <h4 class="text-lg font-semibold">
+                  Ruan Scherer
+                </h4>
+              </div>
+              
+              <p class="mt-3 leading-tight">
+                Fala, pessoal! Vocês viram o lançamento da nova versão do Next.js??? A versão 13 do framework React vem 
+                com tudo e promete muito.
+              </p>
+
+              <a
+                href="post.php?post="
+                class="block w-fit px-4 mt-3 py-1.5 bg-neutral-500/10 text-neutral-50/75 text-sm font-medium rounded-lg hover:bg-emerald-300/10 hover:text-neutral-100 transition"
+              >
+                <i class="fa-regular fa-comment mr-1"></i>
+                Comentar
+              </a>
+            </article>
+          </section>
+        </main>
+      </div>
+    </div>
+
+    <script>
+      function toggleUserPopup() {
+        const userPopup = document.getElementById("user-popup")
+        userPopup.classList.toggle("hidden")
+      }
+    </script>
   </body>
 </html>
